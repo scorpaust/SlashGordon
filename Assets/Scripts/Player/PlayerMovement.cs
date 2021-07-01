250a0f0f1ca196a8a75a29a3045e90cc6f645ed8
Dinis Costa <scorpaust@hotmail.com>
2021-07-01 13:41:48 +0100

    Player animations.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,15 +10,26 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField]
 	private LayerMask groundLayer;
 
+	[SerializeField]
+	private float attackWaitTime = 0.5f;
+
     private Rigidbody2D myBody;
 
 	private Transform groundCheckPos;
 
 	private bool canDoubleJump;
 
+	private bool canAttack;
+
+	private float attackTimer;
+
+	private PlayerAnimationsWithTransitions playerAnim;
+
 	private void Awake()
 	{
 		myBody = GetComponent<Rigidbody2D>();
+
+		playerAnim = GetComponent<PlayerAnimationsWithTransitions>();
 
 		groundCheckPos = transform.GetChild(0).transform;
 	}
@@ -26,6 +37,12 @@ public class PlayerMovement : MonoBehaviour
 	private void Update()
 	{
 		Jump();
+
+		AnimatePlayer();
+
+		GetAttackInput();
+
+		HandleAttackAction();
 	}
 
 	private void FixedUpdate()
@@ -55,6 +72,8 @@ public class PlayerMovement : MonoBehaviour
 				myBody.velocity = Vector2.zero;
 
 				myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
+				playerAnim.PlayDoubleJump();
 			}
 
 			if (IsGrounded())
@@ -63,6 +82,43 @@ public class PlayerMovement : MonoBehaviour
 
 				myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
 			}
+		}
+	}
+
+	private void AnimatePlayer()
+	{
+		playerAnim.PlayJump(myBody.velocity.y);
+
+		playerAnim.PlayFromJumpToRunning(IsGrounded());
+	}
+
+	private void GetAttackInput()
+	{
+		if (Input.GetKeyDown(KeyCode.K))
+		{
+			if (Time.time > attackTimer)
+			{
+				attackTimer = Time.time + attackWaitTime;
+
+				canAttack = true;
+			}
+		}
+	}
+
+	private void HandleAttackAction()
+	{
+		if (canAttack && IsGrounded())
+		{
+			playerAnim.PlayAttack();
+
+			canAttack = false;
+		}
+
+		else if (canAttack && !IsGrounded())
+		{
+			playerAnim.PlayJumpAttack();
+
+			canAttack = false;
 		}
 	}
 }
